92087a71ddabeb950fdbc02f7eab7b6e66d491a7
DeAndre Johnson <dre@phreshr.com>
2019-11-12 07:37:02 +0800

    controls test work

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,49 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Controls from './Controls.js';
+
+describe('Controls component', () => {
+  const mock = jest.fn();
+
+  it('is open/unlock', () => {
+    const { getByText } = render(
+      <Controls locked={false} closed={false} toggleClosed={mock} />,
+    );
+
+    const lockButton = getByText(/^Lock Gate$/i);
+    expect(lockButton).toHaveAttribute('disabled');
+    expect(lockButton.disabled).toBe(true);
+
+    const closeButton = getByText(/^Close Gate$/i);
+    expect(closeButton.disabled).toBe(false);
+    fireEvent.click(closeButton);
+  });
+
+  it('is closed/unlock', () => {
+    const { getByText } = render(
+      <Controls locked={false} closed={true} toggleClosed={mock} />,
+    );
+
+    const lockButton = getByText(/^Lock Gate$/i);
+    expect(lockButton.disabled).toBe(false);
+
+    const openButton = getByText(/^Open Gate$/i);
+    expect(openButton.disabled).toBe(false);
+    fireEvent.click(openButton);
+  });
+
+  it('is closed/locked', () => {
+    const { getByText } = render(
+      <Controls locked={true} closed={true} toggleLocked={mock} />,
+    );
+
+    const unlockButton = getByText(/^Unlock Gate$/i);
+    expect(unlockButton.disabled).toBe(false);
+
+    const openButton = getByText(/^Open Gate$/i);
+    expect(openButton.disabled).toBe(true);
+    fireEvent.click(unlockButton);
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import Display from './Display.js';
@@ -7,6 +7,7 @@ import Display from './Display.js';
 describe('Display component', () => {
   it('should have "red-led" class when locked', () => {
     const { getByText } = render(<Display locked={true} />);
+
     const lockedDisplay = getByText(/locked/i);
     expect(lockedDisplay).toHaveTextContent(/locked/i);
     expect(lockedDisplay).toHaveClass('red-led');
@@ -14,6 +15,7 @@ describe('Display component', () => {
 
   it('should have "green-led" class when unlocked', () => {
     const { getByText } = render(<Display locked={false} />);
+
     const lockedDisplay = getByText(/unlocked/i);
     expect(lockedDisplay).toHaveTextContent(/unlocked/i);
     expect(lockedDisplay).toHaveClass('green-led');
@@ -23,14 +25,13 @@ describe('Display component', () => {
     const { getByText } = render(<Display closed={true} />);
 
     const closedDisplay = getByText(/closed/i);
-
     expect(closedDisplay).toHaveClass('red-led');
-
     expect(closedDisplay).toHaveTextContent(/closed/i);
   });
 
   it('should have green-led class when open', () => {
     const { getByText } = render(<Display closed={false} />);
+
     const closedDisplay = getByText(/open/i);
     expect(closedDisplay).toHaveTextContent(/open/i);
     expect(closedDisplay).toHaveClass('green-led');
